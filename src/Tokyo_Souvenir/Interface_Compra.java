ed843c869369c55eb6486896e6b8e0db5a839ee7
Gustavo Herrero Nunes <51753426+GustavoHerreroNunes@users.noreply.github.com>
2020-01-07 07:17:59 -0300

    v_1.0

--- src/Tokyo_Souvenir/Interface_Compra.java
+++ src/Tokyo_Souvenir/Interface_Compra.java
@@ -1,3 +1,9 @@
+/*Este código visa simular um sistema de compras de uma loja de souvenir
+ * a fictícia TokyoSouvenir, onde o usuário podera escolher produtos da loja
+ * e "pagar" inserindo o nome do titular e número do cartão(ambos fícticios)
+ */
+
+
 package Tokyo_Souvenir;
 
 import javax.swing.JOptionPane;
@@ -6,8 +12,9 @@ import javax.swing.ImageIcon;
 public class Interface_Compra {
 
 	public static void main(String[] args) {
-		ImageIcon Logo = new ImageIcon("Imagens/Logo.png"), Car = new ImageIcon("Imagens/Compra.png");
+		ImageIcon Logo = new ImageIcon("Imagens/Logo.png"), Car = new ImageIcon("Imagens/Compra.png");//Atribuindo imagens (A logo de TokyoSouvenir e um ícone de compras) aos objetos em questão
 		
+		/*Array Unidimensional dos produtos oferecidos*/
 		String Estoque[] = 
 			   {"",
 			    "Camiseta 'I Love Tokyo'",
@@ -26,13 +33,20 @@ public class Interface_Compra {
 				"Miniatura Tokyo Tower",
 				"Porta Níquel Sushi"},
 			   
+			   /*Array Bidimensional com Nomes e Números dos cartões*/
 			   Cards[][] =
 		   		{{"Fernando Lopez","111000"}, 
 		   		{"Julio Cesar","222000"}, 
 		   		{"Alice Alcantara","333000"}, 
 		   		{"Ana Paula","444000"}},
 		   		
-		Carrinho[] = new String[15], Resp ="", Compra[] = {"Finalizar Compra", "Adicionar Produtos"}, Tit[] = new String[2], Opc[] = {"Reenviar", "Cancelar"};
+		Carrinho[] = new String[15], Resp ="", Tit[] = new String[2], 
+		
+		//Array Unidimensionais que serão utilizados para futuros botões nó código
+		Compra[] = {"Finalizar Compra", "Adicionar Produtos"},  Opc[] = {"Reenviar", "Cancelar"};
+		
+		
+		//Array Unidimensional com preços dos produtos oferecidos
 		float Pres[] =
 			   {(float)29.90,
 				(float)29.90,
@@ -50,14 +64,19 @@ public class Interface_Compra {
 				(float)100.00,
 				(float)4.90}, 
 	    Valor[] = new float[15], Total=0;
+	
 		short Quant[] = new short[15], x=0, y=0;
+	
 		int Cont =1, v =0, g[] = new int [2];
+		
 		
 		JOptionPane.showMessageDialog(null, "Bem vindo ao Sistema de Compras da TokyoSouvenir\nLeve um pedacinho de Tokyo com você!!!", "TokyoSuvenir", -1, Logo);
 		 
-		while(x<15 && Cont !=0) {
+		/*Enquanto a quantia de produtos selecionados forem menores que 15 e Cont indicar que o usuário quer adquirir novos itens*/
+		while(x<15 && Cont ==1) {
 			Carrinho[x] = (String) JOptionPane.showInputDialog(null, "Indique quais produtos deseja adicionar ao Carrinho(Lim: 14)\n", "TokyoSouvenir", -1, Car, Estoque, Estoque[0]);
 			
+			/*Se Não for selecionado nenhum item o sistema é encerrado*/
 			if(Carrinho[0]== "") {
 				JOptionPane.showMessageDialog(null, "Obrigado pela visita e\nVolte sempre!!!", "TohyoSouvenir", -1, Logo);
 				System.exit(0);
@@ -71,31 +90,34 @@ public class Interface_Compra {
 			x++;
 		}
 		
+		/*Enquanto não foram verificados os 14 slotes para produtos*/
 		while(y<15) {
 			int p =0;
 			
+			/*Fazendo a comparação entre o item selecionado na posição 'y' e todo o estoque*/
 			for(int z =0; z<15; z++) {
-				if(Carrinho[y] == Estoque[z+1])
+				if(Carrinho[y] == Estoque[z+1])//'z+1' é necessario pelo fato de apenas em 'Estoque' a primeira posição do Array esta vazia para fins visuais
 					p = z;
 			}
 			
-			Valor[y] = Pres[p] * Quant[y];
+			Valor[y] = Pres[p] * Quant[y];//Valor final da compra do(s) item(ns) 'y'
 			Total += Valor[y];
 			
 			y++;
 		}
 		
-		
+			/*Adicionando dados de compra de cada item a uma String para facilitar o informe ao usuário*/
 			for(int z=0; z<15; z++) {
-				if(Carrinho[z]==""|| Carrinho[z]== null)
+				if(Carrinho[z]==""|| Carrinho[z]== null)//Se a posição do carrinho estiver vazia apenas 'pula-se' uma linha
 					Resp +="\n";
-				else
+				else//Se não adicionam-se os respectivos dados referentes ao item 
 					Resp +=Carrinho[z] + ":\n" + Quant[z] + " Uni =>           R$:" + Valor[z] + "\n";
 			}
 			Resp +="------------------------------------\nTotal R$:" + Total;
 			
-			JOptionPane.showMessageDialog(null, Resp, "TokyoSouvenir", -1);
+			JOptionPane.showMessageDialog(null, Resp, "TokyoSouvenir", -1);//Valor final da compra e de cada item é informado ao Usuário
 			
+			/*Peça e processe o nome do títuar e número do cartão...*/
 			do {
 				int r =0;
 				v =0;
@@ -103,10 +125,12 @@ public class Interface_Compra {
 				Tit[0] = JOptionPane.showInputDialog(null, "Insira o nome do títular do cartão:", "TohyoSouvenir", 1);
 				Tit[1] = JOptionPane.showInputDialog(null, "Agora insira o número do cartão de '" + Tit[0] + "'", "TohyoSouvenir", 1);
 				
+				//Verificando se algum dos dados informados constam no sistema
 				for(int a=0; a<4; a++) {
 					
 					for(int s=0; s<2; s++) {
-
+						
+						/*Se a informação em questão consta no sistema*/
 						if(Cards[a][s] == Tit[s].intern()) {
 							v++;
 							g[s] = a;
@@ -115,14 +139,15 @@ public class Interface_Compra {
 					}
 				}
 				
+				/*Se a s informações passadas não constarem no sistema, ou não peretencerem ao mesmo cadastro*/
 				if(v !=2 || g[0] != g[1]) {
 					r =JOptionPane.showOptionDialog(null, "Nome e/ou Cartão inválido(s), \nDeseja reenviar os dados?", "TohyoSouvenir", -1, 0, null, Opc, Opc[0]);
-					if(r==1) { 
+					if(r==1) { //Se não quiser reenviar os dados
 						JOptionPane.showMessageDialog(null, "Obrigado pela visita e\nVolte sempre!!!", "TohyoSouvenir", -1, Logo);
 						System.exit(0);
 					}
 				}
-			}while(v !=2 ||g[0] != g[1]);
+			}while(v !=2 ||g[0] != g[1]);//...enquanto estes não constarem no sistema, ou não peretencerem ao mesmo cadastro
 			
 			
 			JOptionPane.showMessageDialog(null, "Compra Realizada com Sucesso!!!\nObrigado e volte sempre!!!", "TokyoSouvenir", -1, Logo);
